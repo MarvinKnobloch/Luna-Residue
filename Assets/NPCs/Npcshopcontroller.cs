4d8e66a0be629e3f06ee7693d08703589bd790a6
MarvinKnobloch <NivramKnobloch@gmail.com>
2023-01-26 17:06:49 +0100

    npcshopcontroller item selection update

--- Assets/NPCs/Npcshopcontroller.cs
+++ Assets/NPCs/Npcshopcontroller.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Npcshopcontroller : MonoBehaviour
 {
@@ -9,7 +11,8 @@ public class Npcshopcontroller : MonoBehaviour
 
     [SerializeField] private Inventorycontroller matsinventory;
     [SerializeField] private Craftingobject money;
-    [SerializeField] private TextMeshProUGUI moneycount;
+    [SerializeField] private TextMeshProUGUI moneycountdisplay;
+    [SerializeField] private TextMeshProUGUI currentslotdisplay;
     public List<Itemcontroller> npcshopitems = new List<Itemcontroller>();
     private List<GameObject> instaniateditems = new List<GameObject>();
     [SerializeField] private GameObject shopitemprefab;
@@ -18,12 +21,14 @@ public class Npcshopcontroller : MonoBehaviour
     [SerializeField] private TextMeshProUGUI newstats;
     [SerializeField] private TextMeshProUGUI comparestats;
 
-    [SerializeField] private GameObject[] itemslots;
+    [SerializeField] private GameObject[] itemtypeselectionbuttons;
     private Itemtype[] itemtypes = { Itemtype.Sword, Itemtype.Bow, Itemtype.Fist, Itemtype.Head, Itemtype.Chest, Itemtype.Gloves, Itemtype.Legs, Itemtype.Shoes, Itemtype.Neckless, Itemtype.Ring };
     private int currentitemtype;
-    private int maxitemtype;
     private List<Itemtype> listoftypes = new List<Itemtype>();
 
+    [SerializeField] private Inventorycontroller[] inventorys;
+    [SerializeField] private GameObject inventoryitemprefab;
+    [SerializeField] private GameObject playeritemsui;
 
     private void Awake()
     {
@@ -33,26 +38,62 @@ public class Npcshopcontroller : MonoBehaviour
     {
         listoftypes.Clear();
         currentitemtype = 0;
-        maxitemtype = 0;
-        if (money.inventoryslot != 0)  moneycount.text = matsinventory.Container.Items[money.inventoryslot - 1].amount.ToString() + " Money";
-        else  moneycount.text = 0 + " Money";
+
+        if (money.inventoryslot != 0)  moneycountdisplay.text = matsinventory.Container.Items[money.inventoryslot - 1].amount.ToString() + " Money";
+        else  moneycountdisplay.text = 0 + " Money";
         instantiateshopitems();
-        merchantslots();
+        setmerchantslots();
         setcurrentitemtype();
         displayitemtyp();
     }
     private void Update()
     {
-        if (controlls.Menusteuerung.Menucharselectionleft.WasPerformedThisFrame())
-        {
-            if (currentitemtype == maxitemtype) currentitemtype = 0;
-            else currentitemtype++;
-            displayitemtyp();
-        }
         if (controlls.Menusteuerung.Menucharselectionright.WasPerformedThisFrame())
         {
-            if (currentitemtype == 0) currentitemtype = maxitemtype;
-            else currentitemtype--;
+            itemtypeselectionbuttons[currentitemtype].GetComponent<Image>().color = Color.white;
+            currentitemtype++;
+            foreach (GameObject obj in itemtypeselectionbuttons)
+            {
+                if (currentitemtype >= itemtypeselectionbuttons.Length)
+                {
+                    currentitemtype = 0;
+                }
+                if (itemtypeselectionbuttons[currentitemtype].activeSelf == false)
+                {
+                    currentitemtype++;
+                    continue;
+                }
+                else
+                {
+                    itemtypeselectionbuttons[currentitemtype].GetComponent<Image>().color = Color.green;
+                    currentslotdisplay.text = itemtypeselectionbuttons[currentitemtype].GetComponentInChildren<TextMeshProUGUI>().text;
+                    break;
+                }
+            }
+            displayitemtyp();
+        }
+        if (controlls.Menusteuerung.Menucharselectionleft.WasPerformedThisFrame())
+        {
+            itemtypeselectionbuttons[currentitemtype].GetComponent<Image>().color = Color.white;
+            currentitemtype--;
+            foreach (GameObject obj in itemtypeselectionbuttons)
+            {
+                if (currentitemtype < 0)
+                {
+                    currentitemtype = itemtypeselectionbuttons.Length -1;
+                }
+                if (itemtypeselectionbuttons[currentitemtype].activeSelf == false)
+                {
+                    currentitemtype--;
+                    continue;
+                }
+                else
+                {
+                    itemtypeselectionbuttons[currentitemtype].GetComponent<Image>().color = Color.green;
+                    currentslotdisplay.text = itemtypeselectionbuttons[currentitemtype].GetComponentInChildren<TextMeshProUGUI>().text;
+                    break;
+                }
+            }
             displayitemtyp();
         }
     }
@@ -69,10 +110,11 @@ public class Npcshopcontroller : MonoBehaviour
             instaniateditems.Add(obj);
         }
     }
-    private void merchantslots()
+    private void setmerchantslots()
     {
-        foreach(GameObject obj in itemslots)
+        foreach(GameObject obj in itemtypeselectionbuttons)
         {
+            obj.GetComponent<Image>().color = Color.white;
             obj.SetActive(false);
         }
         for (int i = 0; i < instaniateditems.Count; i++)
@@ -80,22 +122,22 @@ public class Npcshopcontroller : MonoBehaviour
             if(listoftypes.Contains(instaniateditems[i].GetComponent<Npcshopselectitem>().merchantitem.type) == false)
             {
                 listoftypes.Add(instaniateditems[i].GetComponent<Npcshopselectitem>().merchantitem.type);
-                maxitemtype++;
             }
         }
-        Debug.Log(maxitemtype);
-        for (int i = 0; i < listoftypes.Count; i++)
+        for (int i = 0; i < itemtypes.Length; i++)
         {
-            if (listoftypes.Contains(itemtypes[i])) itemslots[i].SetActive(true);
+            if (listoftypes.Contains(itemtypes[i])) itemtypeselectionbuttons[i].SetActive(true);
         }
     }
     private void setcurrentitemtype()
     {
-        for (int i = 0; i < itemslots.Length; i++)
+        for (int i = 0; i < itemtypeselectionbuttons.Length; i++)
         {
-            if (itemslots[i].activeSelf == true)
+            if (itemtypeselectionbuttons[i].activeSelf == true)
             {
                 currentitemtype = i;
+                itemtypeselectionbuttons[currentitemtype].GetComponent<Image>().color = Color.green;
+                currentslotdisplay.text = itemtypeselectionbuttons[currentitemtype].GetComponentInChildren<TextMeshProUGUI>().text;
                 return;
             }
             else continue;
@@ -103,13 +145,47 @@ public class Npcshopcontroller : MonoBehaviour
     }
     private void displayitemtyp()
     {
+        EventSystem.current.SetSelectedGameObject(null);
         for (int i = 0; i < instaniateditems.Count; i++)
         {
             if (instaniateditems[i].GetComponent<Npcshopselectitem>().merchantitem.type != itemtypes[currentitemtype])
             {
                 instaniateditems[i].SetActive(false);
             }
+            else
+            {
+                instaniateditems[i].SetActive(true);
+                if(EventSystem.current.currentSelectedGameObject == null)
+                {
+                    EventSystem.current.SetSelectedGameObject(instaniateditems[i]);
+                    instaniateditems[i].GetComponent<Npcshopselectitem>().statsupdate();
+                }
+            }
         }
+        displayinventoryitems();
+    }
+    private void displayinventoryitems()
+    {
+        foreach (Transform obj in playeritemsui.transform)
+        {
+            obj.gameObject.SetActive(false);
+        }
+        /*for (int i = 0; i < inventorys[currentitemtype].Container.Items.Length; i++)
+        {
+            if (inventorys[currentitemtype].Container.Items[i].amount != 0)
+            {
+                //ar obj = Instantiate(inventoryitemprefab, merchantitemslots.transform.position, Quaternion.identity, merchantitemslots.transform);
+                //obj.GetComponentInChildren<TextMeshProUGUI>().text = inventorys[currentitemtype].Container.Items[i].itemname;
+            }
+        }*/
+    }
+    public void clickitemtypebutton(int itemtype)
+    {
+        itemtypeselectionbuttons[currentitemtype].GetComponent<Image>().color = Color.white;
+        currentitemtype = itemtype;
+        itemtypeselectionbuttons[currentitemtype].GetComponent<Image>().color = Color.green;
+        currentslotdisplay.text = itemtypeselectionbuttons[currentitemtype].GetComponentInChildren<TextMeshProUGUI>().text;
+        displayitemtyp();
     }
 }
 
